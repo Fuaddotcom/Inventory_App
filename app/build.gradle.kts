d3bfd25c64ce2b6c67833ffed5a2160bf3a84443
RhapsodyInBlueMelody <syeddinulfaiz14@gmail.com>
2025-04-25 18:13:37 +0700

    [Update]: QR Feature

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,6 +38,7 @@ android {
 
 dependencies {
     implementation(platform("com.google.firebase:firebase-bom:33.12.0"))
+    implementation("com.google.code.gson:gson:2.13.1")
     implementation("com.google.firebase:firebase-auth")
     implementation("com.google.android.gms:play-services-auth:20.7.0")
     implementation("com.google.firebase:firebase-database-ktx:20.3.0")
--- app/src/main/java/com/TI23B1/inventoryapp/ScanActivity.kt
+++ app/src/main/java/com/TI23B1/inventoryapp/ScanActivity.kt
@@ -7,8 +7,17 @@ import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.ActivityResultLauncher
 import androidx.appcompat.app.AppCompatActivity
+import com.google.gson.Gson
+import com.google.gson.annotations.SerializedName
 import com.journeyapps.barcodescanner.ScanContract
 import com.journeyapps.barcodescanner.ScanOptions
+
+data class CargoInfo(
+    @SerializedName("cargoId") val cargoId: String,
+    @SerializedName("type") val type: String,
+    @SerializedName("location") val location: String,
+    @SerializedName("shelf") val shelf: String,
+)
 
 class ScanActivity : AppCompatActivity() {
 
@@ -19,13 +28,31 @@ class ScanActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_scan)
+        scanButton = findViewById(R.id.scanqrbtn)
 
         barcodeLauncher =registerForActivityResult(ScanContract()) { result ->
             if (result.contents == null){
                 Toast.makeText(this, "Canceled", Toast.LENGTH_LONG).show()
             } else {
-                val scannedTagId = result.contents
-                Toast.makeText(this, "Scanned: $scannedTagId", Toast.LENGTH_LONG).show()
+                val scannedData = result.contents
+                try {
+                    val gson = Gson()
+                    val cargoInfo = gson.fromJson(scannedData, CargoInfo::class.java)
+                    val intent = Intent(this, CargoDetailsActivity::class.java)
+
+
+                    intent.putExtra("cargoId", cargoInfo.cargoId)
+                    intent.putExtra("type", cargoInfo.type)
+                    intent.putExtra("location", cargoInfo.location)
+                    intent.putExtra("shelf", cargoInfo.shelf)
+
+
+
+                    startActivity(intent)
+                } catch(e: Exception){
+                    Toast.makeText(this, "Invalid Cargo QR Code", Toast.LENGTH_LONG).show()
+                }
+
             }
         }
         scanButton.setOnClickListener { startQRScan() }
